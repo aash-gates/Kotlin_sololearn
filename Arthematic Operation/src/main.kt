b99c6dc1028b61600243756703963bc88fba3ef8
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-22 13:22:38 +0530

    Update main.kt

--- Arthematic Operation/src/main.kt
+++ Arthematic Operation/src/main.kt
@@ -4,11 +4,11 @@
 fun main(){
     var num1 = 8
     var num2 = 34
-    print(num1 + num2)  //Addition
-    print(num1 - num2)  //Subtraction
-    print(num1 * num2)  //Multiplication
-    print(num2 / num1)  //Division
-    print(num1 % num2) //Modules
+    println(num1 + num2)  //Addition
+    println(num1 - num2)  //Subtraction
+    println(num1 * num2)  //Multiplication
+    println(num2 / num1)  //Division
+    println(num1 % num2) //Modules
 }
 
 //End of the Program
