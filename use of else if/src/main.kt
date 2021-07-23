3a29b6c89eecf09ae86747bff5115d7dc9afddaa
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-23 22:30:08 +0530

    Update main.kt

--- use of else if/src/main.kt
+++ use of else if/src/main.kt
@@ -1,2 +1,14 @@
-class main {
+fun main(){
+    val num = -7
+    when {
+        num>0 -> {
+            println("Positive")
+        }
+        num<0 -> {
+            println("Negative")
+        }
+        else -> {
+            print("zero")
+        }
+    }
 }
