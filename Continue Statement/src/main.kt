3638691408d9007591e262f6e9a661313dc360cc
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-27 21:49:50 +0530

    Update main.kt

--- Continue Statement/src/main.kt
+++ Continue Statement/src/main.kt
@@ -1,13 +1,12 @@
 fun main(){
     var sum = 0
-    var i=1
-    while (i<=100)
+    var i = 1
+    while (i<=100) {
         i++
-        if(%2 !=0) {
+        if(i%2 != 0) {
             continue
+        }
+        sum += i
     }
-    sum +=i
-
-    }
-println(sum)
+    println(sum)
 }
