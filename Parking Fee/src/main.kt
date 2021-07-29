349f42cf58efee57b006f8a211b3fab84960dccc
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-29 15:40:05 +0530

    Update main.kt

--- Parking Fee/src/main.kt
+++ Parking Fee/src/main.kt
@@ -1,16 +1,18 @@
-fun main(args: Array<String>) {
-    var hours = readLine()!!.toInt()
+fun main() {
+    val hours = readLine()!!.toInt()
     var total: Double = 0.0
-    var day = hours /24
+    val day = hours /24
 
     if(hours > 24){
         total = 15*day + (hours%24)*0.5
-    }else if(hours > 5 && hours < 24){
+    }else if(hours in 6..23){
         total = 5 + (hours - 5)*0.5
-    }else if(hours > 0 && hours < 5) {
-        total = hours * 1.0
-    } else{
-        total =1.0
+    }else {
+        if((hours > 0) && (hours < 5)) {
+            total = hours * 1.0
+        } else{
+            total =1.0
+        }
     }
     println(total)
 }
